72c34afc8eb78641ddc12dbd17d2c12b1db62dac
mbbhatti <mbbhatti@gmail.com>
2019-06-18 11:39:19 +0500

    Turing

--- config/test.js
+++ config/test.js
@@ -1,6 +1,6 @@
 var test = {
 	server: "http://localhost:5000",
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImN1c3RvbWVyX2lkIjo0MiwibmFtZSI6ImJhYmFyIiwiZW1haWwiOiJtYmJoYXR0aUBnbWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIifSwiaWF0IjoxNTYwNjY3MTUwLCJleHAiOjE1NjA3NTM1NTB9.w6wdjrS-TflB9sDMsxH9I8LhohD0BhpCWeIUoVv9ltI'
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImN1c3RvbWVyX2lkIjo0NywibmFtZSI6IkJhYmFyIiwiZW1haWwiOiJtYmJoYXR0aUBnbWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIifSwiaWF0IjoxNTYwODM4NjAzLCJleHAiOjE1NjA5MjUwMDN9.fqmSgwvMPUud_3B6LiDG7UZT-tjOX7kL_H9UV4T29ps'
 }
 
 exports.test = test;
